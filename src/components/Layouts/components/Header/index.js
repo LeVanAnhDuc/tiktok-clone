5922c5077482a5bd746a2f8d54bfde19db8ae58b
LeVanAnhDuc <levananhduc1804@gmail.com>
2023-07-04 15:43:40 +0700

    4/7: import fontawesome, create scss logo, scss search-header

--- src/components/Layouts/components/Header/index.js
+++ src/components/Layouts/components/Header/index.js
@@ -1,5 +1,9 @@
 import styles from './Header.module.scss';
 import classNames from 'classnames/bind';
+import images from '../../../../assets/images';
+
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faCircleXmark, faSpinner, faMagnifyingGlass } from '@fortawesome/free-solid-svg-icons';
 
 const cx = classNames.bind(styles);
 
@@ -9,11 +13,23 @@ function Header() {
             <header className={cx('wrapper')}>
                 <div className={cx('inner')}>
                     {/* Logo */}
-                    <div>1</div>
-                    <div>2</div>
-                    <div>3</div>
+                    <img src={images.logo} alt="Image-Tiktok" />
+
                     {/* Search */}
+                    <div className={cx('search')}>
+                        <input placeholder="Tìm kiếm" spellCheck={false} />
+                        <button className={cx('clear')}>
+                            <FontAwesomeIcon icon={faCircleXmark} />
+                        </button>
+                        <FontAwesomeIcon className={cx('loading')} icon={faSpinner} />
+
+                        <button className={cx('search-btn')}>
+                            <FontAwesomeIcon icon={faMagnifyingGlass} />
+                        </button>
+                    </div>
+
                     {/* Setting */}
+                    <div className={cx('action')}>3</div>
                 </div>
             </header>
         </>
