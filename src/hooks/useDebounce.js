3c8cf2c74cec0e7838edc8ce7773c7976860a14f
LeVanAnhDuc <levananhduc1804@gmail.com>
2023-07-21 13:13:34 +0700

    fix warning useDebounce

--- src/hooks/useDebounce.js
+++ src/hooks/useDebounce.js
@@ -7,6 +7,7 @@ function useDebouunce(value, wait) {
         const handle = setTimeout(() => setDebounceValue(value), wait);
 
         return () => clearTimeout(handle);
+        // eslint-disable-next-line react-hooks/exhaustive-deps
     }, [value]);
 
     return debounceValue;
